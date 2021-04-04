fd4dad1c1b0652c19ddc0e7929d5246a6d23d79f
Artiom P <apoluyanovich@gmail.com>
2021-04-04 23:47:31 +0300

    added Navigation and Labels

--- LoginApp/Models/Creators.swift
+++ LoginApp/Models/Creators.swift
@@ -1,3 +1,10 @@
+//
+//  AboutCreatorsViewController.swift
+//  LoginApp
+//
+//  Created by Artiom Poluyanovich on 4.04.21.
+//
+
 struct Creators {
     let creator: String
     let designer: String
